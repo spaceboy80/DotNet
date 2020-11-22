45efe88515f68dbe0e3fc6bc8b682f6ed8cb82ff
spaceboy80 <38818123+spaceboy80@users.noreply.github.com>
2020-11-21 22:47:33 -0500

    Add files via upload

---
+++ Polymorphism/Polymorphism/Circle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Circle : DrawingObject
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("I am a circle.");
+        }
+    }
+}
---
+++ Polymorphism/Polymorphism/DrawingObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class DrawingObject
+    {
+        public virtual void Draw()
+        {
+            Console.WriteLine("I am drawing an object.");
+        }
+    }
+}
